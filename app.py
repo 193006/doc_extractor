9acc0e94c4971f84a4b3a093d16b483f49d5f9e4
193006 <137424083+193006@users.noreply.github.com>
2023-08-01 15:59:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,26 +30,6 @@ from langchain import PromptTemplate, LLMChain
 #from scipy.io.wavfile import write
 from usellm import Message, Options, UseLLM
 
-llm = OpenAI(model_name='text-davinci-003', temperature=0.2, max_tokens=512, openai_api_key=st.secrets["api_key"])
-
-def record_audio(filename):
-    duration=5
-    sample_rate = 44100
-    # Record audio
-    audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=6)
-    st.write('Recording started. Speak into the microphone...')
-    # Wait for recording to complete
-    sd.wait()
-    # Save the recorded audio to a WAV file
-    write(filename, sample_rate, audio_data)
-
-    st.write(f'Recording saved as {filename}')
-    
-def transcribe_audio(filename):
-    with open(filename, "rb") as audio_file:
-        transcript = openai.Audio.transcribe("whisper-1", filename)
-        return transcript["text"]
-
 
 st.title("Suspicious Activity Reporting")
 st.subheader('Evidence Processor')
@@ -75,10 +55,6 @@ def embed(model_name):
     
     return hf_embeddings
 
-# @st.cache_resource
-# def chain(_llm):
-#     chain = load_summarize_chain(llm=_llm, chain_type="map_reduce")
-# chain = load_summarize_chain(llm=llm, chain_type="map_reduce")
 
 hf_embeddings = embed(model_name) 
 
@@ -87,7 +63,6 @@ file = st.file_uploader("Upload a file")
 
 st.write(file)
 text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
     chunk_size = 1500,
     chunk_overlap  = 100,
     length_function = len,
@@ -118,42 +93,98 @@ if st.button("Submit"):
         queries ="Any fraud encountered in the passed document?\
         if any."
         contexts = docsearch.similarity_search(queries, k=1)
-        prompts = f"Give concise answer to the below questions as truthfully as possible as per given context only,\n\n\
-              Context: {contexts}\n\
-              Response (in readable bullet points): "
+        
+        if query.lower() == "what is the victim's name?":
+            prompt_1 = f'''Perform Name Enitity Recognition to identify the Customer name as accurately as possible, given the context. The Customer can also be referenced as the Victim or the person with whom the Fraud has taken place.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        elif query.lower() == "what is the suspect's name?":
+            prompt_1 = f'''Perform Name Enitity Recognition to identify the Suspect name as accurately as possible, given the context. Suspect is the Person who has committed the fraud with the Customer. Respond saying "The Suspect Name is not Present" if there is no suspect in the given context.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        elif query.lower() == "list the merchant name":
+            prompt_1 = f'''Perform Name Enitity Recognition to identify all the Merchant Organizations as accurately as possible, given the context. A merchant is a type of business or organization that accepts payments from the customer account. Give a relevant and concise response.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        elif query.lower() == "how was the bank notified?":
+            prompt_1 = f''' You need to act as a Financial analyst to identify how was the bank notified of the Supicious or Fraud event with in the given context. The means of communication can be a call, an email or in person. Give a relevant and concise response.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        elif query.lower() == "when was the bank notified?":
+            prompt_1 = f''' You need to act as a Financial analyst to identify the when the bank was notified of the Fraud i.e., the disputed date. Given the context, provide a relevant and concise response.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        elif query.lower() == "what type of fraud is taking place?":
+            prompt_1 = f''' You need to act as a Financial analyst to identify the type of fraud or suspicious activity has taken place amd summarize it, within the given context. Also mention the exact fraud code. Give a relevant and concise response.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        elif query.lower() == "when did the fraud occur?":
+            prompt_1 = f''' You need to act as a Financial analyst to identify the when the did the fraud occur i.e., the Transaction Date. Given the context, provide a relevant and concise response.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+            
+        elif query.lower() == "was the disputed amount greater than 5000 usd?":
+            prompt_1 = f''' You need to act as a Financial analyst to identify the disputed amount and perform a mathematical calculation to check if the disputed amount is greater than 5000 or no, given the context. Give a relevant and concise response.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        elif query.lower() == "what type of cards are involved?":
+            prompt_1 = f''' You need to act as a Financial analyst to identify the type of card and card's brand involved, given the context. On a higher level the card can be a Credit or Debit Card. VISA, MasterCard or American Express, Citi Group, etc. are the different brands with respect to a Credit card or Debit Card . Give a relevant and concise response.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        elif query.lower() == "was the police report filed?":
+            prompt_1 = f''' You need to act as a Financial analyst to identify if the police was reported of the Fraud activity, given the context. Give a relevant and concise response.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+            
+        elif query.lower() == "Is this a valid SAR case?":
+            prompt_1 = f''' You need to act as a Financial analyst to check if this is a SAR or not, given the following context, if the transaction amount is less than 5000 USD we cannot categorize this as SAR (Suspicious activity Report).Give a relevant and concise response. \n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\
+                      Response: '''
+
+        
+        else:
+            prompt_1 = f'''Act as a financial analyst and give concise answer to below Question as truthfully as possible, with given Context.\n\n\
+                      Question: {query}\n\
+                      Context: {context_1}\n\                      
+                      Response: '''
+
+            
               
 
-        response = usellm(prompts)
-        # Print the assistant's response
-        # st.subheader("Few informative points in the uploaded document are as follows:")
-        # st.write(response)
+        response = usellm(prompt_1)
 
-#st.write("Uploaded File Contents:")
 if file is not None:
     docs, docsearch = embedding_store(file)
-#docs = text_to_docs(texts)
-#st.write(docs)
-#summary = chain.run(docs)
-#st.write(summary)
-#st.header("Speech and Text Input")
-# Speech Input
-# st.subheader("Speech Input")
-# wav_audio_data = st_audiorec()
 
-# if wav_audio_data is not None: 
-#     st.audio(wav_audio_data, format='audio/wav')
-#     audio_text = transcribe_audio(wav_audio_data)
-#     st.write(audio_text)
-    
-# if st.button("Voice Input"):
-#     rand = random.randint(1, 10000)*random.randint(10001,20000)
-#     audio_filename = f"recorded_audio{rand}.wav"
-   
-#     record_audio(filename=audio_filename)
-#     aud = transcribe_audio(audio_filename)
-#     st.write(aud)
-
-# Text Input
 st.subheader("Ask Questions")
 query = st.text_input('your queries will go here...')
 
@@ -165,24 +196,12 @@ def LLM_Response():
 
         
 if query:
-    # Text input handling logic
-    #st.write("Text Input:")
-    #st.write(text_input)
     context = docsearch.similarity_search(query, k=5)
     prompt = f"Act as a financial analyst and give concise answer to below Question as truthfully as possible, with given Context\n\n\
               Question: {query}\n\
               Context: {context}\n\
               Response: "
 
-    #prompt = PromptTemplate(template=prompt, input_variables=["query", "context"])
     response = usellm(prompt) #LLM_Response()
     st.write(response)
-    # language = 'en'
-    # Create a gTTS object
-    # tts = gTTS(text=response, lang=language)
-    
-    # Save the audio file
-    # rand = random.randint(1, 10000)*random.randint(10001,20000)
-    # audio_file = f'output{rand}.mp3'
-    # tts.save(audio_file)
-    # playsound(audio_file)
+
