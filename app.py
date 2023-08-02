c14e92a43046183a2824aadab11e9821fd3e568f
193006 <137424083+193006@users.noreply.github.com>
2023-08-03 05:14:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,13 +34,18 @@ from usellm import Message, Options, UseLLM
 st.title("Suspicious Activity Reporting")
 st.subheader('Evidence Processor')
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-    #GithubIcon {
-      visibility: hidden;
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
     }
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 model_name = "sentence-transformers/all-MiniLM-L6-v2"
 
