4084a3bddd0429135defaccc5d0348a062b3b5bb
193006 <137424083+193006@users.noreply.github.com>
2023-08-16 21:19:02 +0530

    Update app.py
    embedding changes

--- app.py
+++ app.py
@@ -321,7 +321,10 @@ def embed(model_name):
     hf_embeddings = HuggingFaceEmbeddings(model_name=model_name)
     return hf_embeddings
 
-hf_embeddings = embed(model_name) 
+if pdf_files:
+    hf_embeddings = embed(model_name) 
+else:
+    pass
 
 
 # Vizualising the files
