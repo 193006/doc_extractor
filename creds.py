b6171cfde42d546261580e26e5cb7a18f69baf4f
193006 <137424083+193006@users.noreply.github.com>
2024-04-08 19:43:16 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,1 +1,24 @@
 api_key="rterfdgdfgdgdf"
+
+
+def rouge_n(reference, hypothesis, n):
+
+    ref_ngrams = ngrams(reference, n)
+
+    hyp_ngrams = ngrams(hypothesis, n)
+ 
+    common = ref_ngrams.intersection(hyp_ngrams)
+
+    return len(common) / len(ref_ngrams)
+
+from nltk.translate.bleu_score import sentence_bleu
+def bleu_score(reference, hypothesis):
+    return sentence_bleu([reference], hypothesis)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+ 
+def cos_sim(text1, text2):
+    vectorizer = TfidfVectorizer()
+    tfidf = vectorizer.fit_transform([text1, text2])
+    return cosine_similarity(tfidf[0:1], tfidf[1:2])[0][0]
